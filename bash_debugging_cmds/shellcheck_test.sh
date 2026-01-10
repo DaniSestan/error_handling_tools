6ce8d5da81ecc884fd2bf3c9fe949ea46f3d0520
Dani Sestan <daniasestan@gmail.com>
2026-01-10 16:56:56 +0100

    init commit

--- bash_debugging_cmds/shellcheck_test.sh
+++ bash_debugging_cmds/shellcheck_test.sh
@@ -2,7 +2,7 @@
 
 # ShellCheck is a static analysis tool that can be used for bash code linting
 
-# `shellcheck /home/dani/Work/Work-Projects/error_handling_tools/shellcheck_testing/shellcheck_test.sh`
+# `shellcheck shellcheck_test.sh`
 
 # Sample code:
 sayHello="Hello,"
